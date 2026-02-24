81a971649019028e0a7513d41816063d93859d5d
Leonel <leonelmmorgado@gmail.com>
2026-02-24 17:44:30 -0300

    mouse wkring wayland

--- Scripts/cursor.gd
+++ Scripts/cursor.gd
@@ -1,26 +1,93 @@
-extends Node3D
+extends Node
 
-const VELOCIDADE: float = 500.0
+@export var zona_morta: float = 0.2
+@export var dist: float = 5.0
+@export var velocidade: float = 100.0
+
+var movement: Vector2
+@onready var cursor := $Cursor
+@onready var t_viewport := get_viewport()
+@onready var camera := get_viewport().get_camera_3d()
+@onready var raycast: RayCast3D = RayCast3D.new()
+var viewport_size: Vector2
+var ultimo_colisor = null
+var last_picked_collider_rid = null
+var last_picked_collider: CollisionObject3D = null
+var joystick: bool = false
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	get_viewport().size_changed.connect(_viewport_update_size)
+	_viewport_update_size()
+	cursor.position = get_viewport().get_mouse_position()
+	raycast.enabled = true
+	raycast.collide_with_areas = true
+	add_child(raycast)
+
+func _viewport_update_size() -> void:
+	viewport_size = get_viewport().size
+	t_viewport = get_viewport()
 
 func _input(event: InputEvent) -> void:
-	pass
+	if event is InputEventJoypadMotion:
+		movement = Vector2(Input.get_joy_axis(0, JOY_AXIS_LEFT_X), Input.get_joy_axis(0, JOY_AXIS_LEFT_Y))
+		if movement.length() < zona_morta:
+			movement = Vector2.ZERO
+		else:
+			joystick = true
+			_cursor_move_inject()
+	if event is InputEventMouseMotion:
+		movement = event.relative
+		if movement.length() < zona_morta:
+			movement = Vector2.ZERO
+	if event is InputEventJoypadButton:
+		if event.button_index == JOY_BUTTON_A:
+			_cursor_click_inject(event.pressed)
+			joystick = true
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta: float) -> void:
-	var pos_cursor = get_viewport().get_mouse_position()
-	var mov := Input.get_vector("controle_esquerda", "controle_direita", "controle_cima", "controle_baixo")
+func _process(delta: float) -> void:
+	if movement != Vector2.ZERO:
+		_cursor_move(delta)
 
-	Input.warp_mouse(round(pos_cursor + mov * VELOCIDADE * delta))
+func _physics_process(_delta: float) -> void:
+		raycast.force_raycast_update()
+		if not raycast.is_colliding():
+			if last_picked_collider and last_picked_collider.has_signal("mouse_exited"):
+				last_picked_collider.mouse_exited.emit()
+			last_picked_collider = null
+			last_picked_collider_rid = null
+			return
+		var rid = raycast.get_collider_rid()
+		var collider = raycast.get_collider()
+		if rid != last_picked_collider_rid:
+			if last_picked_collider and last_picked_collider.has_signal("mouse_exited"):
+				last_picked_collider.mouse_exited.emit()
+			if collider.has_signal("mouse_entered"): collider.mouse_entered.emit()
+		last_picked_collider = collider
+		last_picked_collider_rid = rid
+
+func _cursor_move(delta: float) -> void:
+	cursor.position += movement * velocidade * (delta if not joystick else delta * 10.0)
+	cursor.position.x = clamp(cursor.position.x, 0.0, viewport_size.x)
+	cursor.position.y = clamp(cursor.position.y, 0.0, viewport_size.y)
+	if joystick: _cursor_move_inject()
 	
-	if Input.is_action_just_pressed("selecionar"):
-		var click := InputEventMouseButton.new()
-		click.pressed = true
-		click.button_index = MOUSE_BUTTON_LEFT
-		click.position = pos_cursor
-		get_viewport().push_input(click)
-	if Input.is_action_just_released("selecionar"):
-		var click := InputEventMouseButton.new()
-		click.pressed = false
-		click.button_index = MOUSE_BUTTON_LEFT
-		click.position = pos_cursor
-		get_viewport().push_input(click)
+	var origin = camera.project_ray_origin(cursor.position)
+	var direction = camera.project_ray_normal(cursor.position)
+	raycast.global_position = origin
+	raycast.target_position = direction * dist
+
+func _cursor_click_inject(press: bool) -> void:
+	var event: InputEventMouseButton = InputEventMouseButton.new()
+	event.button_mask |= MOUSE_BUTTON_MASK_LEFT
+	event.button_index = MOUSE_BUTTON_LEFT
+	event.pressed = press
+	event.position = cursor.position
+	t_viewport.push_input(event)
+
+func _cursor_move_inject() -> void:
+	var event: InputEventMouseMotion = InputEventMouseMotion.new()
+	event.relative = movement
+	event.screen_relative = movement
+	event.position = cursor.position
+	t_viewport.push_input(event)
