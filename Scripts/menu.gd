06df41b2b86877a81cb0ec4f9c09c7c9769f3564
Leonel <leonelmmorgado@gmail.com>
2026-03-01 13:00:50 -0300

    menu

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -1,5 +1,9 @@
 extends Control
 
+const parallax_1: float = 1.0
+const parallax_2: float = 2.0
+const parallax_attenuation: float = 75.0
+
 @onready var menu := $Menu
 @onready var opts := $Opts
 @onready var vol_slider: HSlider = $Opts/Volume/VBoxContainer/HSlider
@@ -14,6 +18,10 @@ extends Control
 @onready var btn_click_sfx = $btn_click
 var list_btn_click = ["res://Audio/SFX/CLIQUE BOTÕES 1.wav", "res://Audio/SFX/CLIQUE BOTÕES 2.wav"]
 
+@onready var fundo := $fundo
+@onready var contrato := $contrato
+@onready var mao := $mao
+
 var ult_mudan: float
 
 func _ready() -> void:
@@ -21,6 +29,11 @@ func _ready() -> void:
 	vol_slider.value = ult_mudan
 	check_tela.button_pressed = DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN
 	check_sync.button_pressed = DisplayServer.window_get_vsync_mode() == DisplayServer.VSYNC_ENABLED
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+#		contrato.position += event.relative * parallax_1 / parallax_attenuation
+		mao.position += event.relative * parallax_2 / parallax_attenuation
 
 func _on_iniciar_button_down() -> void:
 	init_sound.play()
@@ -34,8 +47,8 @@ func _on_opt_button_down() -> void:
 	click_sfx()
 
 func _on_cred_button_down() -> void:
-	Gerenciador.muda_cena("Menu", "res://Scenes/creditos.tscn")
 	click_sfx()
+	Gerenciador.muda_cena("Menu", "res://Scenes/creditos.tscn")
 
 func _on_sair_button_down() -> void:
 	click_sfx()
